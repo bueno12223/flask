d17ce9845f7da57f6b7d5f3aa0baca0d0a3178a3
Jesus Berrio <berriojesus122@gmail.com>
2021-08-08 11:01:21 -0500

    templates agregados

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
-
+from flask import Flask, request, make_response, redirect, render_template
 app = Flask(__name__)
 
+todos = ['ver el curso', 'revisar email', 'leer docs']
+
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('ip', user_ip)
+    return response
+
+@app.route('/hello')
 def hello():
-    return "hello world flask"
+    user_ip = request.cookies.get('ip')
+    context = {
+        'user_ip':user_ip, 
+        'todos':todos
+    }
+    return render_template('file.html', **context)
