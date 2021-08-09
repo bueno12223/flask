d664541df4398fbd168ed04d9f4ea6a7a6f30051
Jesus Berrio <berriojesus122@gmail.com>
2021-08-09 16:00:37 -0500

    flask login implementado correctamente

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import request, make_response, redirect, render_template, url_for, session
 from app import create_app
 from app.forms import LoginForm
+from flask_login import login_required
 from app.firestore_service import get_users, get_todos
 import unittest
 
@@ -24,6 +25,7 @@ def index():
     return response
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
     username = session.get('username')
